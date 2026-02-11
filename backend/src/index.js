a76f8d4d8576da42687c37db044d81aa24fa87d0
sachin <sachinjangir1319@gmail.com>
2026-02-12 02:20:08 +0530

    Update CORS origin to production URL

--- backend/src/index.js
+++ backend/src/index.js
@@ -23,7 +23,7 @@ app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://full-stack-chat-app-2-frontend.onrender.com",
     credentials: true,
   })
 );
