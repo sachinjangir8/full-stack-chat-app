fd8d6191058e195a6c5b6d7a1bc51a5979e643f7
SachinJangir8 <sachinjangir1319@gmail.com>
2026-02-14 13:01:06 +0530

    ui changes

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -12,9 +12,13 @@ const HomePage = () => {
       <div className="flex items-center justify-center pt-20 px-4">
         <div className="bg-base-100 rounded-lg shadow-cl w-full max-w-6xl h-[calc(100vh-8rem)]">
           <div className="flex h-full rounded-lg overflow-hidden">
-            <Sidebar />
+            <div className={`${selectedUser ? "hidden lg:block" : "block"} h-full`}>
+              <Sidebar />
+            </div>
 
-            {!selectedUser ? <NoChatSelected /> : <ChatContainer />}
+            <div className={`${!selectedUser ? "hidden lg:block" : "block"} flex-1 w-full`}>
+              {!selectedUser ? <NoChatSelected /> : <ChatContainer />}
+            </div>
           </div>
         </div>
       </div>
