cd1be8ad7996c1d7319cfdf3e894d2e1f94a0eda
SachinJangir8 <sachinjangir1319@gmail.com>
2026-02-25 01:03:09 +0530

    bug fixing

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -124,9 +124,9 @@ const SignUpPage = () => {
             <div className="flex flex-col items-center gap-2 group">
               <div
                 className="size-12 rounded-xl bg-primary/10 flex items-center justify-center 
-              group-hover:bg-primary/20 transition-colors"
+              group-hover:bg-primary/20 transition-colors overflow-hidden"
               >
-                <MessageSquare className="size-6 text-primary" />
+                <img src="/logo.png" alt="Logo" className="size-10 object-contain" />
               </div>
               <h1 className="text-2xl font-bold mt-2">Create Account</h1>
               <p className="text-base-content/60">Get started with your free account</p>
